3ab46dd3e9ee54e977ecfbdbd40e1da8d7254c5f
Thanh Nguyen <nvthanh1804@gmail.com>
2021-12-21 23:34:07 +0900

    feat: test build hash map

---
+++ map.js
@@ -0,0 +1,68 @@
+class HashMap {
+    constructor() {
+        this._map = [];
+    }
+
+    /**
+      * hash string to hash value
+      * @var string stringKey
+      * @returns hash
+      */
+    hashKey(stringKey) {
+        let hash = 0;
+
+        for (let i = 0; i < stringKey.length; i++) {
+            const charCode = stringKey.charCodeAt(i);
+
+            hash = ((hash << 5) - hash) + charCode;
+
+            hash |= 0;
+        }
+
+        return hash
+    }
+
+    /**
+      * set new item into array hash map
+      * @var string key
+      * @var any value
+      * @returns void
+      */
+    set(key, value) {
+        let id = this.hashKey(key);
+
+        if (!this._map[id]) {
+            this._map[id] = [];
+        }
+
+        this._map[id].push([key, value]);
+    }
+
+    /**
+      * get value by key, return undefined if don't have value
+      * @var string key
+      * @returns value
+      */
+    get(key) {
+        let id = this.hashKey(key);
+
+        if (!this._map[id]) {
+            return undefined;
+        }
+
+        for (const keyVal of this._map[id]) {
+            if (keyVal[0] === key) {
+                return keyVal[1];
+            }
+        }
+    }
+}
+
+const hashMap = new HashMap()
+
+hashMap.set("Thanh", { a: 'ok' })
+
+const value = hashMap.get("Thanh")
+const value1 = hashMap.get("Th")
+
+console.log(value, value1)
